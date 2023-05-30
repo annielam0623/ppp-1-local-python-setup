5553ea9a22d596e16f4d4eab8899c4cf1d796895
annielam0623 <114551177+annielam0623@users.noreply.github.com>
2023-05-29 22:53:48 -0700

    Update functions_practice.py
    update unsaved file

--- functions_practice.py
+++ functions_practice.py
@@ -1,3 +1,14 @@
 def hello():
     print('Hello user!')
+
+def pack(arg1, arg2, arg3):
+    return [arg1, arg2, arg3]
+
+def eat_lunch(food_list):
+    if len(food_list) == 0:
+        print("My lunch box is empty!")
+    else:
+        print("first I eat ", food_list[0])
+        for food in food_list[1:]:
+            print("Next I eat ", food)
     
